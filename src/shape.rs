e806aa3dd4f01545f0e4174bc206f3d275e78de2
Stephan Luther <kvsari@gmail.com>
2019-06-26 23:17:39 +0900

    Add octahedron

--- src/shape.rs
+++ src/shape.rs
@@ -182,9 +182,6 @@ pub fn tetrahedron(len: f32, colour: [f32; 3]) -> ([Vertex; 12], [u16; 12]) {
     let top_point_normal = average_normals(&[n1, n2, n4]);
     let depth_point_normal = average_normals(&[n2, n3, n4]);
 
-    // debug colour
-    let dcolour: [f32; 3] = [0f32, 0f32, 1f32];
-
     /*
     let vertexes = [
         // T1
@@ -236,6 +233,93 @@ pub fn tetrahedron(len: f32, colour: [f32; 3]) -> ([Vertex; 12], [u16; 12]) {
         3, 4, 5,
         6, 7, 8,
         9, 10, 11,
+    ];
+
+    (vertexes, indexes)
+}
+
+pub fn octahedron(len: f32, colour: [f32; 3]) -> ([Vertex; 24], [u16; 24]) {
+    // We want to build the anchor square in the center (0, 0, 0) over X, Y.
+    let h_len: f32 = len / 2f32;
+
+    // We spell out the formula instead of using `half_len` to avoid confusion.
+    let circumscribed_sphere_radius: f32 = (len / 2f32) * 2f32.sqrt();
+
+    // Build our square.
+    let p_top_left:  [f32; 3] = [h_len.neg(), h_len, 0f32];
+    let p_top_right: [f32; 3] = [h_len, h_len, 0f32];
+    let p_bot_left:  [f32; 3] = [h_len.neg(), h_len.neg(), 0f32];
+    let p_bot_right: [f32; 3] = [h_len, h_len.neg(), 0f32];
+
+    // Get the Z points using the sphere radius
+    let p_far:  [f32; 3] = [0f32, 0f32, circumscribed_sphere_radius.neg()];
+    let p_near: [f32; 3] = [0f32, 0f32, circumscribed_sphere_radius];
+
+    // Calc our normals!
+    let n1 = triangle_normal([&p_top_left, &p_top_right, &p_far]);
+    let n2 = triangle_normal([&p_bot_left, &p_top_left, &p_far]);
+    let n3 = triangle_normal([&p_bot_right, &p_bot_left, &p_far]);
+    let n4 = triangle_normal([&p_top_right, &p_bot_right, &p_far]);
+    
+    let n5 = triangle_normal([&p_top_right, &p_top_left, &p_near]);
+    let n6 = triangle_normal([&p_top_left, &p_bot_left, &p_near]);
+    let n7 = triangle_normal([&p_bot_left, &p_bot_right, &p_near]);
+    let n8 = triangle_normal([&p_bot_right, &p_top_right, &p_near]);
+
+    // debug colour
+    let dcolour: [f32; 3] = [0f32, 0f32, 1f32];
+
+    let vertexes = [
+        // T1
+        Vertex::new(p_top_left, n1, colour),
+        Vertex::new(p_top_right, n1, colour),
+        Vertex::new(p_far, n1, colour),
+
+        // T2
+        Vertex::new(p_bot_left, n2, colour),
+        Vertex::new(p_top_left, n2, colour),
+        Vertex::new(p_far, n2, colour),
+
+        // T3
+        Vertex::new(p_bot_right, n3, colour),
+        Vertex::new(p_bot_left, n3, colour),
+        Vertex::new(p_far, n3, colour),
+
+        // T4
+        Vertex::new(p_top_right, n4, colour),
+        Vertex::new(p_bot_right, n4, colour),
+        Vertex::new(p_far, n4, colour),
+
+        // T5
+        Vertex::new(p_top_right, n5, colour),
+        Vertex::new(p_top_left, n5, colour),
+        Vertex::new(p_near, n5, colour),
+
+        // T6
+        Vertex::new(p_top_left, n6, colour),
+        Vertex::new(p_bot_left, n6, colour),
+        Vertex::new(p_near, n6, colour),
+
+        // T7
+        Vertex::new(p_bot_left, n7, colour),
+        Vertex::new(p_bot_right, n7, colour),
+        Vertex::new(p_near, n7, colour),
+
+        // T8
+        Vertex::new(p_bot_right, n8, colour),
+        Vertex::new(p_top_right, n8, colour),
+        Vertex::new(p_near, n8, colour),
+    ];
+
+    let indexes = [
+        0,  1,  2,
+        3,  4,  5,
+        6,  7,  8,
+        9,  10, 11,
+        12, 13, 14,
+        15, 16, 17,
+        18, 19, 20,
+        21, 22, 23,
     ];
 
     (vertexes, indexes)
